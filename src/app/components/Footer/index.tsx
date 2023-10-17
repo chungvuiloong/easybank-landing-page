f63fb0e7912471bc33d55aa290c2ca65304d8a74
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-17 09:19:56 +0300

    Added opacity to text.

--- src/app/components/Footer/index.tsx
+++ src/app/components/Footer/index.tsx
@@ -26,20 +26,20 @@ const index = () => {
                         </div>
                         <div className='basis-1/2 flex md:flex-row flex-col'>
                             <div className='basis-1/2 flex flex-col gap-5'>  
-                                <div className='md:text-left text-center'>About Us</div>
-                                <div className='md:text-left text-center'>Contact</div>
-                                <div className='md:text-left text-center'>Blog</div>
+                                <div className='md:text-left text-center text-White opacity-70'>About Us</div>
+                                <div className='md:text-left text-center text-White opacity-70'>Contact</div>
+                                <div className='md:text-left text-center text-White opacity-70'>Blog</div>
                             </div>
                             <div className='basis-1/2 flex flex-col gap-5'>  
-                                <div className='md:text-left text-center'>Careers</div>
-                                <div className='md:text-left text-center'>Support</div>
-                                <div className='md:text-left text-center'>Privacy Policy</div>
+                                <div className='md:text-left text-center text-White opacity-70'>Careers</div>
+                                <div className='md:text-left text-center text-White opacity-70'>Support</div>
+                                <div className='md:text-left text-center text-White opacity-70'>Privacy Policy</div>
                             </div>
                         </div>
                     </div>
                     <div className='basis-1/2 flex flex-col justify-between'>
                         <button className='px-8 py-2 md:self-end self-center bg-gradient-to-r from-Lime-Green to-Bright-Cyan text-White rounded-full'>Request Invite</button>
-                        <div className='text-Light-Grayish-Blue md:self-end self-center'>  © Easybank. All Rights Reserved</div>
+                        <div className='text-Light-Grayish-Blue md:self-end self-center opacity-50'>  © Easybank. All Rights Reserved</div>
                     </div>
                 </div>
             </div>

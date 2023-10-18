2f5f07f31a996372b092a3bf905b0c05ced439a3
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-18 08:24:00 +0300

    Added padding for container in all views

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,15 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
+      },
+    },
     extend: {
       colors: {
         'Dark-Blue': 'hsl(233, 26%, 24%)',

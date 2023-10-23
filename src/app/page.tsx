8e43079dca5ecd249dfc4157aced32fa7e58c4c5
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-23 15:24:47 +0300

    Added components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,18 @@
 import Image from 'next/image'
 import Navbar from '@/components/Navbar'
+import HeroBanner from '@/components/HeroBanner'
+import Choose from '@/components/Choose'
+import LatestArticle from '@/components/LatestArticle'
+import Footer from '@/components/Footer'
 
 export default function Home() {
   return (
     <main>
         <Navbar />
-
+        <HeroBanner />
+        <Choose />
+        <LatestArticle />
+        <Footer />
     </main>
   )
 }

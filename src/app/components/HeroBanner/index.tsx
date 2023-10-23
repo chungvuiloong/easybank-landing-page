c14c93b977f0c24de28d056d441fda9dff9d3f47
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-23 16:08:37 +0300

    Made herobanner look like Figma

--- src/app/components/HeroBanner/index.tsx
+++ src/app/components/HeroBanner/index.tsx
@@ -1,16 +1,33 @@
+import Image from 'next/image';
 import React from 'react';
+import HeroBannerBGImage from '@/assets/images/bg-intro-desktop.svg'
+import FrontImage from '@/assets/images/image-mockups.png'
+import Button from '@/components/Button'
 
 const index = () => {
     return (
-        <section className='bg-White py-24'>
+        <section className='bg-White overflow-hidden flex flex-row '>
             <div className='container mx-auto'>
                 <div className='flex flex-row'>
-                    <div className='basis-1/2 flex flex-col md:gap-6 gap-4'>                
+                    <div className='basis-1/2 flex flex-col justify-center md:gap-6 gap-4'>                
                         <h2 className='md:text-4xl text-3xl opacity-80'>Next generation digital banking</h2>
                         <p className='text-Dark-Blue opacity-50 md:text-[17px] text-xs leading-[26px]'>Take your financial life online. Your Easybank account will be a one-stop-shop for spending, saving, budgeting, investing, and much more.</p>
+                        <div className='flex flex-row flex-start'>
+                            <Button />
+                        </div>
                     </div>
-                    <div className='basis-1/2'>    
-                    Image
+                    <div className="basis-1/2 mx-auto -mt-24 max-w-5xl 
+                    lg:ml-10 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32">
+                        <div className="relative lg:max-w-none sm:max-w-5xl max-w-3xl flex-none ">
+                            <Image
+                            src={HeroBannerBGImage}
+                            alt="App screenshot"
+                            width={2432}
+                            height={1442}
+                            className="absolute w-[76rem]"
+                            />
+                            <Image src={FrontImage} alt={"Mobile phones"} className='relative'/>
+                        </div>
                     </div>
                 </div>
             </div>

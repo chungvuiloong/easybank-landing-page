f64fa61ebdf0e610bd4f2c3b9d9c401a20eff5e2
Jere Sulovuo <35882808+jeresulovuo@users.noreply.github.com>
2023-10-26 22:09:17 +0300

    Removed className

--- src/app/components/Button/index.tsx
+++ src/app/components/Button/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const index = () => {
-    return <button className={`${className} md:flex hidden  px-6 py-3 md:self-end self-center bg-gradient-to-r from-Lime-Green to-Bright-Cyan text-White rounded-full text-xs font-medium`}>Request Invite</button>
+    return <button className={`md:flex hidden  px-6 py-3 md:self-end self-center bg-gradient-to-r from-Lime-Green to-Bright-Cyan text-White rounded-full text-xs font-medium`}>Request Invite</button>
 };
 
 export default index;
